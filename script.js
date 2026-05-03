d5f12c69b067c8c6423ff1486aacef164a0ff1a9
shalinipolavaram-debug <shalini.polavaram@gmail.com>
2026-05-03 23:26:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const weddingDate = new Date("May 4, 2026 10:00:00").getTime();
+
+setInterval(function() {
+  const now = new Date().getTime();
+  const gap = weddingDate - now;
+
+  const days = Math.floor(gap / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+  document.getElementById("countdown").innerHTML =
+    "⏳ " + days + " days " + hours + " hours left";
+}, 1000);
